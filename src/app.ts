9c9e14ef1519520bb69b12d48cdf9b13d77ab0ca
Dirlan Ferreira <dirlan2014@yahoo.com.br>
2023-08-16 05:30:16 -0300

    feat: added proxys and endpoints

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,58 @@
 import "dotenv/config";
 import fastify from "fastify";
-import cors from '@fastify/cors'
+import cors from "@fastify/cors";
+import proxy from "@fastify/http-proxy";
+import { replyStreamData } from "./utils";
 
 const server = fastify();
 server.register(cors, {
-  origin: true //accepting all
-})
+  origin: true, //accepting all
+});
 
 server.get("/", (request, reply) => {
   reply.send("Server Running");
+});
+
+server.register(proxy, {
+  upstream: process.env.GITHUB_API + "/users",
+  prefix: "/api/users",
+  http2: false,
+  replyOptions: {
+    onResponse: (request, reply, res) => {
+      const { since } = request.query as { since: string };
+      const { url: endpoint } = request.routeConfig;
+      const appUrl = request.raw.rawHeaders[1];
+      const perPage = 30; // default amount from docs
+      const nextSince = (isNaN(Number(since)) ? 0 : Number(since)) + perPage;
+      const nextPage = `http://${appUrl}${endpoint}?since=${nextSince}`;
+
+      replyStreamData(request, reply, res, { nextPage });
+    },
+  },
+});
+
+server.register(proxy, {
+  upstream: process.env.GITHUB_API + "/users",
+  prefix: "/api/users/:username/details",
+  rewritePrefix: "/users/:username",
+  http2: false,
+  replyOptions: {
+    onResponse: (request, reply, res) => {
+      replyStreamData(request, reply, res);
+    },
+  },
+});
+
+server.register(proxy, {
+  upstream: process.env.GITHUB_API + "/users",
+  prefix: "/api/users/:username/repos",
+  rewritePrefix: "/users/:username/repos",
+  http2: false,
+  replyOptions: {
+    onResponse: (request, reply, res) => {
+      replyStreamData(request, reply, res);
+    },
+  },
 });
 
 server.listen({ port: Number(process.env.PORT || "3000") }, (err, address) => {
